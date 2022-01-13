9a1e96aa22f5a28125839fa834701eabe1f3bbc7
August Wittgenstein <august.wittgenstein@tum.de>
2022-01-13 23:51:48 +0100

    Move explanation block

---
+++ frontend/src/components/explanationBlock.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Page, Navbar, Block } from 'framework7-react';
+
+const ExplanationBlock = () => (
+<Block strong>
+    <p>
+      Mit <b>WoDone</b> kannst du Bilder ganzer Fantasy-Welten erstellen, die
+      dich beim Entwerfen von Geschichten und Storylines inspirieren und
+      unterstützen.
+    </p>
+    <p>
+      Die Bilder werden dabei anhand deiner Präferenzen immer besser und
+      passender. Um deine Präferenzen mitzuteilen, halte auf einem Bild
+      gedrückt, um es zu liken. Alle gelikten Bilder kannst du dir später in
+      deiner Suchhistorie anschauen.
+    </p>
+  </Block>
+);
+
+export default ExplanationBlock;
