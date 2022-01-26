6c1927a105c3b2a1ff6372ab6dcc54309f9c360f
Tobias Höpp <43952751+t0bska@users.noreply.github.com>
2022-01-26 16:02:18 +0100

    add curl commands

--- backend/backend/ai/install.sh
+++ backend/backend/ai/install.sh
@@ -1,3 +1,5 @@
-git clone https://github.com/openai/CLIP
-#git clone https://github.com/CompVis/tamming-transformers.git
+#git clone https://github.com/openai/CLIP
+#git clone https://github.com/CompVis/taming-transformers.git
 #git clone https://github.com/minimaxir/icon-image.git
+curl -L -o vqgan_imagenet_f16_16384.ckpt -C - 'https://heibox.uni-heidelberg.de/f/867b05fc8c4841768640/?dl=1'
+curl -L -o vqgan_imagenet_f16_16384.yaml -C - 'https://heibox.uni-heidelberg.de/f/274fb24ed38341bfa753/?dl=1'
