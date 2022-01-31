386fde75455bf5d34272fa2776f7819851eaf59f
Bernhard Stöffler <bernhard.stoeffler@tum.de>
2022-01-31 04:23:27 +0100

    dictionary: more adjectives

--- backend/backend/vqgan/wodone_mod_words.py
+++ backend/backend/vqgan/wodone_mod_words.py
@@ -1,1 +1,8 @@
-wodone_words = ["red", "green", "blue"]
+wodone_words = [
+    "red", "green", "blue", "yellow", "orange", "purple", "magenta", "cyan", "black", "white",
+    "big", "small", "tiny", "huge", "giant",
+    #"skyrim", "witcher", "dishonored", "half life", "portal",
+    "cyberpunk", "steampunk", "utopian", "dystopian", "nordic", "futuristic", "medieval", "realistic",
+    "horror", "gloomy", "calming", "warming", "cold", "energetic", "technical", "electric", "scientific",
+    "broken", "ruined", "clean", "burning", "flooded", "water", "floating"
+]
