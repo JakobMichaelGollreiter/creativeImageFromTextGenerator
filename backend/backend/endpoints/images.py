df33c34ff97059b457ef8235e51216e4a304850f
August Wittgenstein <august.wittgenstein@tum.de>
2022-02-04 20:20:46 +0100

    bug fix

--- backend/backend/endpoints/images.py
+++ backend/backend/endpoints/images.py
@@ -3,15 +3,11 @@ from main import api
 from vqgan.get_image import get_image
 @api.route("/api/requestimage", methods=["POST"])
 def requestImage():
-	data = request.json()
-	
-    #here the search information from the frontend is safed
+    data = request.json()
     imPath, status = get_image(data["searchString"],data["reqArray"])
-    
-    #here it is passed to the backend 
+
     return jsonify({
         "status": status,
         "imgPath": imgPath
     }), 200
 	
-
