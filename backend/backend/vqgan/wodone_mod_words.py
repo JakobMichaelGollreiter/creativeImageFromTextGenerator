3b21d3deace7682d140e756862502e758ff4ebba
Bernhard Stöffler <bernhard.stoeffler@tum.de>
2022-02-05 21:26:04 +0100

    revisited wodone_mod_words

--- backend/backend/vqgan/wodone_mod_words.py
+++ backend/backend/vqgan/wodone_mod_words.py
@@ -1,8 +1,28 @@
 wodone_adjectives = [
-    "red", "green", "blue", "yellow", "orange", "purple", "magenta", "cyan", "black", "white",
-    "big", "small", "tiny", "huge", "giant",
-    #"skyrim", "witcher", "dishonored", "half life", "portal",
-    "cyberpunk", "steampunk", "utopian", "dystopian", "nordic", "futuristic", "medieval", "realistic",
-    "horror", "gloomy", "calming", "warming", "cold", "energetic", "technical", "electric", "scientific",
-    "broken", "ruined", "clean", "burning", "flooded", "water", "floating"
+#colors
+    "red", "green", "blue", "yellow", "orange", "purple", "magenta", "cyan", "black", "white", #big impact
+    #"big", "small", "tiny", "huge", "giant",                   #don't work very well
+#themes
+    #"skyrim", "witcher", "dishonored", "half life", "portal",  #work very well but beware infringement
+    "cyberpunk", "steampunk",                                   #fantastic results
+    "utopian", "dystopian",                                     #too vague somehow
+    "futuristic", "future",                                     #less impact than expected
+    "medieval", "ancient", "prehistoric",                       #sometimes works
+    "realistic",                                                #not actually realistic but still interesting
+#cultural stuff
+    "japanese", "chinese", "asian",
+    "mediterranian", "african", "south american",
+    "nordic", "northern",                                       #good for landscapes, otherwise not much
+#emotions
+    "horror", "bloody", "terrifying", "scary",                  #nice twist
+    "gloomy", "mysterious", "magical", "fantasy",               #medium/good results
+    #"calming",                                                 #little impact
+    "warming", "cold",
+    #"energetic", "technical", "electric", "scientific",        #little impact
+    "overgrown", "natural", "underground",                      #overgrown works well, the rest, meh
+    "broken", "ruined", "dirty", "destroyed", "abandoned"       #medium impact
+    "clean", "sterile", "pure", "heavenly",                     #little impact
+    "burning", "fire", "smoking",                               #big impact
+    "flooded", "water", "underwater", "sunken",                 #sometimes big impact
+    "floating", "flying"
 ]
