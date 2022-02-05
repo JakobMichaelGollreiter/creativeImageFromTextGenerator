20b41f93605b8c7d59a3ba2b0cb6a4b8e4ba4f6e
Bernhard Stöffler <bernhard.stoeffler@tum.de>
2022-02-05 22:33:06 +0100

    get_image: fixed logic to better detect unfinished images

--- backend/backend/vqgan/get_image.py
+++ backend/backend/vqgan/get_image.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from vqgan.wodone_mod_words import wodone_adjectives
+from wodone_mod_words import wodone_adjectives
 import os
 
 databasepath = "./images"
@@ -11,13 +11,12 @@ def get_image(prompt, modifiers):
         imgpath = imgpath + "/" + wodone_adjectives[int(wordindex)]
 
     imgfilepath = Path(imgpath + "/image.png")
-    if imgfilepath.exists():
-        if Path(imgpath + "/unfinished").exists():
-            print("Image is currently being generated, please be patient")
-            status = 1
-        else:
-            print("Found image")
-            status = 0
+    if imgfilepath.exists() and not Path(imgpath + "/unfinished").exists():
+        print("Found image")
+        status = 0
+    elif Path(imgpath + "/unfinished").exists():
+        print("Image is currently being generated, please be patient")
+        status = 1
     else:
         print("Not found, generating new image")
         status = 1
