94764db0b46f5667afeb50b0f70be833b5f85975
Tobias Höpp <43952751+t0bska@users.noreply.github.com>
2022-02-06 20:13:42 +0100

    seperate dockers for flask and vqgan

---
+++ backend/entrypoint-vqgan.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Function for a clean shutdown of the container
+function shutdown {
+    #kill -TERM "$NGINX_PROCESS" 2>/dev/null
+    exit
+}
+trap shutdown SIGTERM
+
+while ! mysqladmin ping -h"$MYSQL_HOST" --silent; do
+    sleep 1
+done
+
+sleep 10
+
+cd /usr/local/bin/vqgan
+#python3 initialRunner.py
+#TODO
+
