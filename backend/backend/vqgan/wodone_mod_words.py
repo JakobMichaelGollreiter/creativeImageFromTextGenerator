ebb653b7ed2369c41c6de98cff3afa2abdbe4c92
Jakob Gollreiter <jakob.gollreiter@tum.de>
2022-02-11 16:34:49 +0800

    adjustment of mod_words for like algo

--- backend/backend/vqgan/wodone_mod_words.py
+++ backend/backend/vqgan/wodone_mod_words.py
@@ -1,6 +1,6 @@
 wodone_adjectives = [
 #colors
-    "red", "green", "blue", "yellow", "orange", "purple", "magenta", "cyan", "black", "white", #big impact
+    "red", "green", "blue", "yellow", "orange", "purple", "magenta", "cyan", "brown", "black", "white", "gold", "silver" #big impact
     #"big", "small", "tiny", "huge", "giant",                   #don't work very well
 #themes
     #"skyrim", "witcher", "dishonored", "half life", "portal",  #work very well but beware infringement
@@ -10,19 +10,19 @@ wodone_adjectives = [
     "medieval", "ancient", "prehistoric",                       #sometimes works
     "realistic",                                                #not actually realistic but still interesting
 #cultural stuff
-    "japanese", "chinese", "asian",
+    "japan", "china", "asia",
     "mediterranian", "african", "south american",
     "nordic", "northern",                                       #good for landscapes, otherwise not much
 #emotions
     "horror", "bloody", "terrifying", "scary",                  #nice twist, "bloody" works very well
     "gloomy", "mysterious", "magical", "fantasy",               #medium/good results
     #"calming",                                                 #little impact
-    "warming", "cold",
+    "warming", "cold", "snow" ,"desert",
     #"energetic", "technical", "electric", "scientific",        #little impact
     "overgrown", "natural", "underground",                      #overgrown works well, the rest, meh
     "broken", "ruined", "dirty", "destroyed", "abandoned",      #medium impact
     "clean", "sterile", "pure", "heavenly",                     #little impact
     "burning", "fire", "smoking",                               #big impact
     "flooded", "water", "underwater", "sunken",                 #sometimes big impact
-    "floating", "flying"
+    "floating", "flying" , "fly"
 ]
