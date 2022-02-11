cb44fb4743b6b9d05ceb1ffe0fdcb2f34590db96
Jakob Gollreiter <jakob.gollreiter@tum.de>
2022-02-11 16:04:42 +0800

    adjustments in the like backend words

--- backend/backend/vqgan/wodone_mod_words.py
+++ backend/backend/vqgan/wodone_mod_words.py
@@ -1,8 +1,10 @@
  wodone_adjectives = [
     "red", "green", "blue", "yellow", "orange", "purple", "magenta", "brown", "cyan", "black", "white", "silver" , "gold"
-    "big", "small", "tiny", "huge", "giant",
-    #"skyrim", "witcher", "dishonored", "half life", "portal",
-    "cyberpunk", "steampunk", "utopian", "dystopian", "nordic", "futuristic", "medieval", "realistic",
-    "horror", "gloomy", "calming", "warming", "cold", "energetic", "technical", "electric", "scientific",
-    "broken", "ruined", "clean", "burning", "flooded", "water", "floating"
+    "big", "small", "tiny", "huge", "giant", "heavy"
+    "cyberpunk", "steampunk", "utopian", "dystopian", "nordic", "future", "medieval", "realistic",
+    "horror", "gloomy", "calming", "warming", "cold", "energy", "technical", "electric", "science",
+    "broken", "ruined", "clean", "fire", "flooded", "water", "floating"
+    "skyrim", "portal"
+
 ]
+    #"skyrim", "witcher", "dishonored", "half life", "portal"
